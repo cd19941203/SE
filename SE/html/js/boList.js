2384c369a53d197dfaa42c095a917b13775eab2a
dixoon0 <dixoon0@gmail.com>
2017-12-26 00:13:45 +0800

    Testing 'Bootstrap modal'

--- SE/html/js/boList.js
+++ SE/html/js/boList.js
@@ -1,8 +1,23 @@
-function removeList(){
-	$(".accept").click( 
-		function(){
-			$(this).parent().parent().parent().parent().fadeOut(400);
-		}
-	);
+//The List Button
+var ACCEPT = ".accept";
+var EDIT = ".edit";
+var REFUSE = ".refuse";
+
+function btnRemoveList(test,btn_switch){
+	if(btn_switch)//bind 'click' event
+	{
+		$(test).click( 
+			function(){
+				$(this).parent().parent().parent().parent().fadeOut(400);
+			}
+		);
+	}
+	else //unbind 'click' event
+	{
+		$(test).unbind("click");
+	}
 }
-addEventListener("load",removeList,false);
+function init(){
+	btnRemoveList(REFUSE, true);
+}
+addEventListener("load",init,false);
