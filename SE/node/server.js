b932829725461a2cbb7ac7ca1a208afc8be72617
EdenChien <eden4011@yahoo.com.tw>
2017-12-25 17:24:07 +0800

    socket 邏輯 prototype

--- SE/node/server.js
+++ SE/node/server.js
@@ -4,20 +4,18 @@ var session = require("express-session");
 var app = express();
 var server = Server(app);
 var sio = require("socket.io")(server);
-
+var bodyParser = require('body-parser');
+var formData = require("express-form-data");
+var socket = require('./order.js');
 var account = require('./account.js');
 var database = require('./database.js');
 var meal = require('./meal.js');
 
 var rootPath = '../www/';
-
-var bodyParser = require('body-parser');
-var formData = require("express-form-data");
 var multipartyOptions = {
 	autoFiles: true
 };
  
-
 async function init(){
 	
 	////////////////////////////////////////////////////////////
@@ -38,35 +36,96 @@ async function init(){
 		secret: "keyboard cat",
 		cookie:{maxAge:60*1000}
 	});
-	
+
 	sio.use(function(socket, next) {
 		sessionMiddleware(socket.request, socket.request.res, next);
 	});
+	
 	app.use(sessionMiddleware);
 	////////////////////////////////////////////////////////////
 	
 	// middleware for checking user authentication
 	// 路徑不包含loginCheck的request 都會跑這個function檢查有沒有登入OUO
+	
 	app.use(/^(?:(?!loginCheck).)*$/,(req,res,next)=>{
 		if(!(req.session.valid==true))
 			res.sendFile('login.html',{root:rootPath});
 		else
 			next();
 	});
+	
 	////////////////////////////////////////////////////////////
-	// start here
+	//---------------------- start here ----------------------//
+
+	// about socket 
+
+	sio.sockets.on('connection', (socket)=>{
+		console.log('new socket connection');
+		console.log(socket.request.session.account);
+		
+		
+
+		// if account is boss
+		// 查出所有還沒有得到boss ack的訂單 全部再送一次
+
+
+
+		// 把各個user加入個別的room,即可一次對該user的所有socket emit
+		// unfortunately socket.join is asynchronous... 
+		socket.join(socket.request.session.account,()=>{
+			
+			// from customer
+			socket.on('newOrder',(data)=>{
+				var newOrder = JSON.parse(data);
+				console.log(newOrder['test']);
+				//await order.newOrder(data); 在DB紀錄新增該筆訂單
+				// to boss
+				sio.to('boss').emit('newOrder',newOrder);
+			});
+
+			// from boss
+			socket.on('orderAck',(data)=>{
+				var orderAck = JSON.parse(data);
+				// 更新DB訂單狀態  老闆已接到
+				// to customer
+				sio.to(orderAck['user']).emit('orderAck',orderAck);
+			});
+
+			// from boss
+			socket.on('orderComplete',(data)=>{
+				var orderComplete = JSON.parse(data);
+				// 更新DB訂單狀態   餐點已完成
+				// to customer
+				sio.to(orderComplete['user']).emit('orderComplete',orderComplete);
+			});
+
+			// from boss // 以取餐 訂單完成
+			socket.on('orderEnd',(data)=>{
+				var orderEnd = JSON.parse(data);
+				// 更新DB訂單狀態   結單
+				// to customer
+				sio.to(orederEnd['user']).emit('orderEnd',orderComplete);
+			});
+		});	
+    });
+
+
+
+	////////////////////////////////////////////////////////////
+	// about web server
 	
 	app.post('/loginCheck',(req,res)=>{
 		var account = req.body.account;
 		var password = req.body.password;
 		console.log(req.body);
-		res.send("123");
+		res.sendFile('index.html',{root:rootPath});
 	});
 	
 	// 用來假裝登入der
 	app.get('/loginCheck',(req,res)=>{
 		req.session.valid = true;
 		req.session.account = req.query.account;
+		console.log(req.session.account);
 		res.send('hello');
 	});
 	
@@ -78,6 +137,14 @@ async function init(){
 		res.sendFile('test.html',{root:rootPath});
 	});
 	
+	// testing socket
+
+	app.get('/client',(req,res)=>{
+		res.sendFile('client.html',{root:rootPath});
+
+	});
+	//
+
 	app.get('/getMenu',async(req,res)=>{
 		try{
 			var result = await meal.getMenu();
@@ -87,10 +154,9 @@ async function init(){
 		}
 	});
 	
-	app.listen(8787,()=>{
-		console.log('server gogo OUO!')}
-	);
-
+	server.listen(8787,()=>{
+		console.log('server gogo OUO!');
+	});
 }
 
 init();
--- SE/www/js/client.js
+++ SE/www/js/client.js
@@ -1,9 +1,14 @@
+var socket;
 function init(){
-	
+	socket = io.connect('localhost:8787');
+	socket.on('res',(data)=>{
+		document.getElementById("output1").innerHTML = data;
+	});
 }
 addEventListener("load",init,false);
 function test1(){
-	var DIV1 = document.getElementById("output1").innerHTML = "Click";
+	var DIV1 = document.getElementById("output1");
+	socket.emit('newOrder','{"test":8787}');
 }
 function test2(){
 	var DIV2 = document.getElementById("output2").innerHTML = "Click";
