12ef6d09c26d8c274211c8a6619ae17b51c0413a
EdenChien <eden4011@yahoo.com.tw>
2017-12-27 16:45:14 +0800

    socket 更新成新版DER

--- SE/node/order.js
+++ SE/node/order.js
@@ -1,71 +1,122 @@
 var database = require('./database.js');
+var dbConnectionError = 'Database conneciton error';
+var dbManipulationError = 'Database error 0x12';
+var dataError = 'orderNumerError';
+var orderStatus = {
+    new:'new',
+    accepted:'accepted',
+    pending:'pending',
+    canceled:'canceled',
+    completed:'completed',
+    done:'done'
+};
 
-async function newOrder(order){
-    try{
-        var db = await database.connect();
-        db.collection('order').insertOne(order,(err,result)=>{
-            if(err)
-                conosle.log(err);
-        });
-    }catch(err){
-        console.log(err);
-    }
-}
-
-async function orderAck(order){
-    try{
-        var db = await database.connect();
-        db.collection('order').updateOne({orderNumber:order['orderNumber']},{$set:order},(err,result)=>{
-            if(err)
-                console.log(err);
-        });
-    }catch(err){
-        console.log(err);
-    }
-}
-
-async function orderComplete(order){
-    try{
-        var db = await database.connect();
-        db.collection('order').updateOne({orderNumber:order['orderNumber']},{$set:order},(err,result)=>{
-            if(err)
-                console.log(err);
-        });
-    }catch(err){
-        console.log(err);
-    }
-}
-
-async function orderEnd(order){
-    try{
-        var db = await database.connect();
-        db.collection('order').updateOne({orderNumber:order['orderNumber']},{$set:order},(err,result)=>{
-            if(err)
-                console.log(err);
-        });
-    }catch(err){
-        console.log(err);
-    }
-}
-
-async function getNotAckOrder(){
+async function getOrderData(num){
     try{
         var db = await database.connect();
         return new Promise((res,rej)=>{
-            db.collection('order').find({status:'new'}).toArray((err,result)=>{
+            db.collection('order').findOne({orderNumber:num},(err,result)=>{
                 if(err)
-                    rej(err);
+                    rej(dbManipulationError);
                 else
                     res(result);
             });
         });
     }catch(err){
-        return {};
+        throw(dbConnectionError);
     }
 }
 
+async function newOrder(order){
+    try{
+        var db = await database.connect();
+        return new Promise((res,rej)=>{
+            db.collection('order').insertOne(order,(err,result)=>{
+                if(err)
+                    rej(dbManipulationError);
+                else
+                    res();
+            });
+        });
+    }catch(err){
+        throw(dbConnectionError);
+    }
+}
+
+async function orderStatusChange(num,newStatus){
+    try{
+        var db = await database.connect();
+        return new Promise((res,rej)=>{
+            db.collection('order').updateOne({orderNumber:num},{$set:{status:newStatus}},(err,result)=>{
+                if(err)
+                    rej(dbManipulationError);
+                else
+                    res();
+                    /*if(result.result.nModified == 0){
+                        rej()
+                    }*/
+            });
+        });
+    }catch(err){
+        throw(dbConnectionError);
+    }
+}
+
+async function updateModifyAdvice(num,advice){
+    try{
+        var db = await database.connect();
+        return new Promise((res,rej)=>{
+            db.collection('order').updateOne({orderNumner:num},{$set:{advice:advice}},(err,result)=>{
+                if(err)
+                    rej(dbManipulationError);
+                else
+                    res();
+            });
+        });
+    }catch(err){
+        throw(dbConnectionError);
+    }
+}
+
+async function changeOrder(orderData){
+    try{
+        var db = await database.connect();
+        return new Promise((res,rej)=>{
+            db.collection('order').updateOne({orderNumber:orderData['orderNumber']},{$set:orderData},(err,result)=>{
+                if(err)
+                    rej(dbManipulationError);
+                else
+                    res();
+            });
+        });
+    }catch(err){
+        throw(dbConnectionError);
+    }
+}
+
+async function getNewOrder(){
+    try{
+        var db = await database.connect();
+        return new Promise((res,rej)=>{
+            db.collection('order').find({status:'new'},{projection:{_id:0}}).toArray((err,result)=>{
+                if(err)
+                    rej(dbManipulationError);
+                else
+                    res(result);
+            });
+        });
+    }catch(err){
+        throw(dbConnectionError);
+    }
+}
+
+
+module.exports.orderStatus = orderStatus;
+module.exports.dbConnectionError = dbConnectionError;
+module.exports.dbManipulationError = dbManipulationError;
+module.exports.getOrderData = getOrderData;
 module.exports.newOrder = newOrder;
-module.exports.orderAck = orderAck;
-module.exports.orderComplete = orderComplete;
-module.exports.orderEnd = orderEnd;
-module.exports.getNotAckOrder = getNotAckOrder;
+module.exports.orderStatusChange = orderStatusChange;
+module.exports.updateModifyAdvice = updateModifyAdvice;
+module.exports.changeOrder = changeOrder;
+module.exports.getNewOrder = getNewOrder;
--- SE/node/test.js
+++ SE/node/test.js
@@ -9,16 +9,50 @@ async function init(){
 				console.log(i.password);
 			}
 		});
-		console.log(conn);
 	}catch(err){
 		console.log(err);
 		return ;
 	}
 }
+async function orderAccept(num){
+    try{
+        var db = await database.connect();
+        return new Promise((res,rej)=>{
+            db.collection('order').updateOne({orderNumber:27},{$set:{status:'accepted'}},(err,result)=>{
+				console.log(result.result.nModified);
+				if(err)
+                    rej(dbError);
+                else
+                    res();
+            });
+        });
+    }catch(err){
+        throw(dbError);
+    }
+}
 
-init();
+//init();
+orderAccept();
 
 
+/*
+console.log('-----');
+
+database.connect().then(db=>{
+	db.collection('user').find({account:'root'}).toArray((err,result)=>{
+		console.log(result);
+	});
+}).catch(err=>{
+	console.log(err);
+});
+
+try{
+	JSON.parse('{8as7d}');
+}catch(err){
+	console.log(typeof err);
+}
+
+*/
 /*
 db.collection('User').find({account:'root'}).toArray((err,result)=>{
 	for(i of result){
