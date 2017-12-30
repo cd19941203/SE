6465128884d7244a7a2d271d93751137234c3ad4
StarFish <cd19941203@yahoo.com.tw>
2017-12-30 21:48:06 +0800

    no message

--- SE/test/node/test_account.js
+++ SE/test/node/test_account.js
@@ -7,21 +7,13 @@ var account = require('../../node/account.js');
 
 describe('Testing account.js', function () {
 
-    describe('etAccountType( account )', function () {
-        it('getAccountType( root )', async function () {
-            assert.equal(await account.getAccountType('root'), 'boss');
-        });
-
-        it('getAccountType( boss )', async function () {
-            assert.equal(await account.getAccountType('boss'), 'boss');
-        });
-
-        it('getAccountType( client )', async function () {
-            assert.equal(await account.getAccountType('client'), null);
-        });
+    it('getAccountType', async function () {
+        assert.equal(await account.getAccountType('root'), 'boss');
+        assert.equal(await account.getAccountType('boss'), 'boss');
+        assert.equal(await account.getAccountType('client'), null);
     });
 
-    it('login( account, password )', async function () {
+    it('login', async function () {
         assert.equal(await account.login('root', 'root'), true);
     });
 
