26019c026c747145f1f937af3b432502098767c1
StarFish <cd19941203@yahoo.com.tw>
2017-12-30 22:37:52 +0800

    no message

---
+++ SE/test/node/test_server.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var assert = chai.assert;
+var expect = require('chai').expect;
+
+var account = require('../../node/account.js');
+
+
+describe('Testing account.js', function () {
+    /*
+    it('login', async function () {
+        assert.equal(await account.login('root', 'root'), true);
+    });
+    */
+});
