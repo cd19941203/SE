10b9c09cbc441db8c109926a38d2c79eed6ebd91
dixoon0 <dixoon0@gmail.com>
2018-01-06 00:41:46 +0800

    no message

--- SE/html/js/cuMenu.js
+++ SE/html/js/cuMenu.js
@@ -1,2 +1,32 @@
+var debugMode = false;
 //--------------------------- Function about Trigger---------------------------//
 function btnTrigger(){
+	$('.btn-elect').click(function(){
+		$('#MenuPage').hide();
+		$('#orderPage').show();
+	});
+	//[OrderPage] add and reduce order number
+	$('.btn-add').click(function(){
+		if(debugMode)console.log('btn-add click');
+		$(this).siblings('.order-number').html(function(index,oldvalue){
+			return parseInt(oldvalue)+1;
+		});
+	});
+	$('.btn-reduce').click(function(){
+		if(debugMode)console.log('btn-reduce click');
+		$(this).siblings('.order-number').html(function(index,oldvalue){
+			if(parseInt(oldvalue)>1)return parseInt(oldvalue)-1;
+			return 1;
+		});
+	});
+	$('#addOrder').click(function(){
+		$('#MenuPage').show();
+		$('#orderPage').hide();
+	});
+	//
+}
+
+function init(){
+	btnTrigger();
+}
+addEventListener('load', init, false);
---
+++ SE/html/js/cuMenuData.js
@@ -0,0 +1,10 @@
+example = [{"type" : "套餐","name" : "蛋餅套餐","item" : ["起司蛋餅", "紅茶(大杯)"], "price" : 40}];
+var data = [];
+var updateMenu(myData)
+{
+	data = [];
+	for(int i=0;i<myData.length;i++)
+	{
+		//data[ myData[i].type ][]
+	}
+}
