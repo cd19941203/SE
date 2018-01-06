7e8933b20ed5c0f1b14e8ed32ba77a5098bd8650
dixoon0 <dixoon0@gmail.com>
2018-01-06 14:33:39 +0800

    edit cuMenu

--- SE/html/js/cuMenu.js
+++ SE/html/js/cuMenu.js
@@ -1,24 +1,29 @@
-var debugMode = false;
+var debugMode = true;
 //--------------------------- Function about Trigger---------------------------//
 function btnTrigger(){
+	$('.btn-elect').unbind('click');
 	$('.btn-elect').click(function(){
-		$('#MenuPage').hide();
-		$('#orderPage').show();
+		viewOrderPage( $(this).attr('id') );
 	});
 	//[OrderPage] add and reduce order number
+	$('.btn-add').unbind('click');
 	$('.btn-add').click(function(){
 		if(debugMode)console.log('btn-add click');
 		$(this).siblings('.order-number').html(function(index,oldvalue){
 			return parseInt(oldvalue)+1;
 		});
+		updateSumPrice();
 	});
+	$('.btn-reduce').unbind('click');
 	$('.btn-reduce').click(function(){
 		if(debugMode)console.log('btn-reduce click');
 		$(this).siblings('.order-number').html(function(index,oldvalue){
 			if(parseInt(oldvalue)>1)return parseInt(oldvalue)-1;
 			return 1;
 		});
+		updateSumPrice();
 	});
+	$('#addOrder').unbind('click');
 	$('#addOrder').click(function(){
 		$('#MenuPage').show();
 		$('#orderPage').hide();
@@ -28,5 +33,9 @@ function btnTrigger(){
 
 function init(){
 	btnTrigger();
+	data = example;
+	viewCategory = 1;
+	updateData(data);
+	
 }
 addEventListener('load', init, false);
--- SE/html/js/cuMenuData.js
+++ SE/html/js/cuMenuData.js
@@ -1,10 +1,122 @@
-example = [{"type" : "套餐","name" : "蛋餅套餐","item" : ["起司蛋餅", "紅茶(大杯)"], "price" : 40}];
+var viewStatus = "menu";
+var viewCategory = 0;
+var example = [{"type":"套餐","name":"蛋餅套餐","item":["起司蛋餅","紅茶(大杯)"],"price":40,"inventory":true,"image":null},{"type":"蛋餅","name":"起司蛋餅","price":25,"inventory":true,"image":null},{"type":"蛋餅","name":"原味蛋餅","price":20,"inventory":true,"image":null}];
 var data = [];
-var updateMenu(myData)
+var category = [];
+var menu = [];
+
+
+function updateData(myData)
 {
-	data = [];
-	for(int i=0;i<myData.length;i++)
+	category = [];
+	menu = [];
+	for(var i=0; i < myData.length ; i++)
 	{
-		//data[ myData[i].type ][]
+		if(menu[ myData[i].type ] == undefined)
+		{
+			if(debugMode)console.log('add a category name is '+myData[i].type);
+			menu[ myData[i].type ] = [];
+			category.push(myData[i].type);
+		}
+		menu[ myData[i].type ][ myData[i].name ] = i;
 	}
+	
+	updateMenu(viewCategory);
+	btnTrigger();
+}
+function updateMenu(){
+	var str="";
+	for (var categoryList in menu[ category[viewCategory] ]){
+		var mydata = menu[ category[viewCategory] ][ categoryList ];
+		str += 
+		'                    <div class="col-sm-4 col-lg-4 col-md-4">                                                         '+
+		'                        <div class="thumbnail">                                                                      '+
+		'                            <img src="image/null.jpg" alt="">                                                        '+
+		'                            <div class="caption">                                                                    '+
+		'                                <h4 class="pull-right">NT$ '+ (data[mydata].price).toLocaleString('en-US') +'</h4>  '+
+		'                                <h4>                                                                                 '+
+		'									<span class = "glyphicon glyphicon-heart" style = "color:#B22222;"></span>        '+
+		'									<B>' + data[mydata].name + '</B>                                               '+
+		'                                </h4>                                                                                '+
+		'                                <p>                                                                                  '+
+		'沒有內容OUO'+                                                                                                        
+		'								</p>                                                                                  '+
+		'								<hr style = "margin:0px;">                                                            '+
+		'                            </div>                                                                                   '+
+		'                            <div class="ratings">                                                                    '+
+		'								<button class = "btn btn-success pull-right btn-elect" id = "'+mydata+'">訂購</button>'+
+		'								<br><br>                                                                              '+
+		'                            </div>                                                                                   '+
+		'                        </div>                                                                                       '+
+		'                    </div>                                                                                           ';
+	}
+	$('#MenuPage').html(str);
+}
+
+
+
+
+
+//function about Order Page
+function viewOrderPage(orderID, isEdit = false, myEdit = []){
+	if(debugMode)console.log("click " + orderID + " 訂購");
+	viewStatus = "order";
+	
+	$('#OP_id').html(orderID);
+	$('#OP_name').html(data[orderID].name);
+	$('#OP_price').html( (data[orderID].price).toLocaleString('en-US') );
+	updateOption();
+	
+	
+	$('#MenuPage').hide();
+	$('#orderPage').show();
+}
+function updateSumPrice(){
+	$('#OP_sumPrice').html( data[$('#OP_id').html()].price * parseInt($('#singleOrder').html()));
+}
+
+var btnStr = Object.freeze({
+	count: 
+	'<div class = "optionChoose" style = "padding : 5px;">                                                                                                     '+
+	'	<button class = "btn btn-success btn-add" style = "height:30px;padding:0px;width:30px;">＋</button>                                                    '+
+	'	<span class = "panel panel-default order-number" style = "margin-left:10px;margin-right:10px;width:30px;text-align: right;">1</span>                   '+
+	'	<button class = "btn btn-danger btn-reduce" style = "height:30px;padding:0px;width:30px;">－</button>                                                  '+
+	'</div>                                                                                                                                                    ',
+	singleOrder: 
+	'<div class = "optionChoose" style = "padding : 5px;">                                                                                                     '+
+	'	<button class = "btn btn-success btn-add" style = "height:30px;padding:0px;width:30px;">＋</button>                                                    '+
+	'	<span class = "panel panel-default order-number" id = "singleOrder" style = "margin-left:10px;margin-right:10px;width:30px;text-align: right;">1</span>'+
+	'	<button class = "btn btn-danger btn-reduce" style = "height:30px;padding:0px;width:30px;">－</button>                                                  '+
+	'</div>                                                                                                                                                    '});
+//function about Order Page 'Option'
+//input example:   [['單點','count'],['套餐','count']];
+function updateOption(  myOption = [['單點','singleOrder']]  , isEdit = false, myEdit = []){
+	var mystr = "";
+	
+	for(var i=0;i<myOption.length;i++)
+	{
+		mystr+="<span>\n";
+		
+		mystr+='<span class = "mytitle" id = "Option' + i + '">'+ myOption[i][0] +'</span>：<br>';
+		mystr+=btnStr[ myOption[i][1] ];
+		mystr+="</span>\n";
+	}
+	
+	$('#orderOption').html(mystr);
+	if(isEdit)
+	{
+		for(var i=0;i<myEdit.length;i++)
+		{
+			setOption(i,myEdit[i]);
+		}
+	}
+	btnTrigger();
+}
+function setOption(optionIndex,value = 1)
+{
+	return $('#Option'+optionIndex).siblings('.optionChoose').children('.order-number').html(value);
+}
+function getOption(optionIndex)
+{
+	return $('#Option'+optionIndex).siblings('.optionChoose').children('.order-number').html();
 }
